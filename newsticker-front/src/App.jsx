535e127083bedd542394d52acf052a588b76bf92
byBlair <jkas8233@naver.com>
2025-02-19 23:21:32 +0900

    메인페이지 생성 및 추가

--- newsticker-front/src/App.jsx
+++ newsticker-front/src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import NewsPage from './pages/NewsPage';
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<NewsPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ newsticker-front/src/pages/NewsPage.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+function NewsPage(){
+    return (
+        <div>
+            <h1>메인 뉴스 페이지</h1>
+        </div>
+    )
+}
+
+export default NewsPage;
