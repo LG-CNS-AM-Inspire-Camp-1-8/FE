3521d809fab4c2c6c6eee5e650cdc67c35643aa6
binys21 <binys1121@ewhain.net>
2025-02-24 11:13:03 +0900

    feat: 마이페이지 모달 연결

--- newsticker-front/src/components/UserEditFormModal.jsx
+++ newsticker-front/src/components/UserEditFormModal.jsx
@@ -1,23 +1,41 @@
-import { useState } from 'react';
-import '../styles/Modal.css';
+import { useState } from "react";
+import "../styles/Modal.css";
 
-function UserEditFormModal({onClose}) {
-  return(
-      <div className="modal" onClick={onClose}>
-          <div className="modal-content">
-            <h2>게시글 작성</h2>
-            <form>
-                <img src='../assets/icons/profile.png'></img>
-                <label className='modal_form'>회원정보</label>
-                <input type='text'className = 'textname' placeholder="닉네임을 입력하세요" required />
-                <label className='modal_form'>아이디</label>
-                <input type="url" className = 'textname' placeholder="아이디를 입력하세요" />
-                
-                <button type="button" class="upload-button">회원정보 수정하기</button>
-            </form>
-            <button className ='closebtn' onClick={onClose}>X</button>
-          </div>
+function UserEditFormModal({ onClose }) {
+  const handelBackgroundClick = (e) => {
+    if (e.target.classList.contains("model")) {
+      onClose();
+    }
+  };
+  return (
+    <div className="modal" onClick={handelBackgroundClick}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <h2>회원 정보 수정</h2>
+        <form>
+          <img src="../assets/icons/profile.png"></img>
+          <label className="modal_form">회원정보</label>
+          <input
+            type="text"
+            className="textname"
+            placeholder="닉네임을 입력하세요"
+            required
+          />
+          <label className="modal_form">아이디</label>
+          <input
+            type="url"
+            className="textname"
+            placeholder="아이디를 입력하세요"
+          />
+
+          <button type="button" class="upload-button">
+            회원정보 수정하기
+          </button>
+        </form>
+        <button className="closebtn" onClick={onClose}>
+          X
+        </button>
       </div>
+    </div>
   );
 }
-export default BoardFormModal
+export default UserEditFormModal;
