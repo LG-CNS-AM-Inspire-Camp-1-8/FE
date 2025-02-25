ac2684b24b7a6a25eca45f0cddefa6e36027fb3e
byBlair <jkas8233@naver.com>
2025-02-25 18:58:49 +0900

    save boardpage

--- newsticker-front/src/components/NewsDetailModal.jsx
+++ newsticker-front/src/components/NewsDetailModal.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import BoardFormModal from "../components/BoardFormModal";
 import "../styles/Modal.css";
 import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
